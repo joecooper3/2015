6370ac9a466d541d0a11d2550a18f4ce2247706d
joecooper3 <joecooper3@gmail.com>
2016-01-02 11:29:52 -0500

    End of Week
    Mostly About template.

---
+++ wp-content/themes/caseslegit/template-parts/content-about.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template used for displaying the about pages.
+ *
+ * @package CASES Legit
+ */
+
+?>
+<?php
+$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), $thumb_original, false, '' ); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <style type="text/css" media="screen">
+        .serv-bgimage { 
+         background: url(<?php echo $src[0]; ?>); 
+         background-size: cover; 
+         background-position: center;
+        }
+    </style>
+<div class="about-container">
+    <div class="leadimage-container3">
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">Our ' ); ?></h1>
+        </header><!-- .entry-header --></div>
+	<div class="entry-content-about">
+            <?php the_post_thumbnail('news_xl'); ?>
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'caseslegit' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+        <div class="sidebar-about"></div>
+</div>
+	<footer class="entry-footer">
+		<?php edit_post_link( esc_html__( 'Edit', 'caseslegit' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
