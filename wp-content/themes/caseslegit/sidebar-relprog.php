0cc34acf6d01d6a3fb529d7b8a4a13f3bab82ec8
joecooper3 <joecooper3@gmail.com>
2016-02-19 18:48:11 -0500

    Work in the home
    giegue

---
+++ wp-content/themes/caseslegit/sidebar-relprog.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The sidebar for the about pages
+ *
+ * @package CASES Legit
+ */
+
+if ( ! is_active_sidebar( 'sidebar-4' ) ) {
+	return;
+}
+?>
+	<?php dynamic_sidebar( 'sidebar-4' ); ?>
+<!-- #secondary -->
