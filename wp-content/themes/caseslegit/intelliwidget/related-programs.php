7a7043c928f8c5e0d8fd47d283c39a2b71cc59bb
joecooper3 <joecooper3@gmail.com>
2016-02-24 20:30:45 -0500

    Couple days
    Fixed program pages, made some styling changes on service pages

--- wp-content/themes/caseslegit/intelliwidget/related-programs.php
+++ wp-content/themes/caseslegit/intelliwidget/related-programs.php
@@ -13,23 +13,25 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * @access public
  */
 ?>
-<div class="relprog-title">Related Programs</div>
+<div class="relprog-title">Related Programs
+    <div class="relprog-line"></div>
+</div>
 <?php
 if ( $selected->have_posts() ) : while ( $selected->have_posts() ) : $selected->the_post();
 ?>
 <?php $ohbrother = get_the_intelliwidget_title(); ?>
-<div id="intelliwidget_<?php the_intelliwidget_ID(); ?>" class="intelliwidget4-container">
+<a href="<?php the_intelliwidget_url() ?>"><div id="intelliwidget_<?php the_intelliwidget_ID(); ?>" class="intelliwidget4-container">
 <h1 id="intelliwidget_title_<?php the_intelliwidget_ID(); ?>" class="intelliwidget4-title widget-title">
-    <?php the_intelliwidget_link(NULL, "$ohbrother", NULL); ?>
+    <?php echo $ohbrother; ?>
   </h1>
 
   <?php if ( has_intelliwidget_image() ) : ?>
   <div class="intelliwidget-album-container-<?php echo $instance[ 'image_size' ];?> intelliwidget-align-<?php echo $instance[ 'imagealign' ]; ?>">
-    <?php the_intelliwidget_image(); ?>
-      <div class="intelli-about-link"> <?php the_intelliwidget_link(0, "Read more <i class='fa fa-arrow-right'></i>"); ?></div>
+    <?php the_intelliwidget_image(false); ?>
+      <div class="intelli-about-link"> Read more <i class='fa fa-arrow-right'></i></div>
   </div>
   <?php endif; ?>
   
   <div style="clear:both"></div>
-</div>
+    </div></a>
 <?php endwhile; endif; ?>
--- wp-content/themes/caseslegit/template-parts/content-program.php
+++ wp-content/themes/caseslegit/template-parts/content-program.php
@@ -26,7 +26,27 @@ $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), $thumb_ori
 			) );
 		?>
 	</div><!-- .entry-content -->
-        <div class="sidebar-about">Only one can win
+        <div class="sidebar-about sidebar-program">
+            <aside>
+                <?php if( get_field('box1') ): ?>
+                <span class="asidetitle"><?php the_field('box1_title') ?></span>
+            <?php the_field('box1') ?></aside>
+                <?php endif; ?>
+            <?php if( get_field('box2') ): ?>
+            <aside>
+                <span class="asidetitle"><?php the_field('box2_title') ?></span>
+                <p align="center">
+    <a href="<?php echo get_template_directory_uri ()?>/images/proglit/<?php the_field('box2img') ?>.pdf">
+    <img srcset="<?php echo get_template_directory_uri ()?>/images/proglit/<?php the_field('box2img') ?>1x.jpg 1x,
+         <?php echo get_template_directory_uri ()?>/images/proglit/<?php the_field('box2img') ?>2x.jpg 2x"
+         src="<?php echo get_template_directory_uri ()?>/images/proglit/<?php the_field('box2img') ?>1x.jpg" 
+         alt="<?php the_title(); ?> <?php the_field('box2_title') ?>"></p>
+    <p align="center"> <?php the_field('box2') ?></a></p></aside>
+            <?php endif; ?>
+            <?php if( get_field('box3') ): ?>
+            <aside><span class="asidetitle"> <?php the_field('box3_title') ?></span>
+            <?php the_field('box3') ?></aside>
+            <?php endif; ?>
 </div>
 </div>
     
