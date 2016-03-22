21511bad979b5f82006c572d13b6941fe930a76b
Joe Cooper <Joe.Cooper@casesnyc.local>
2016-03-22 13:53:55 -0400

    smdh
    some caldera forms updates and also new way of doing the referral page

--- wp-content/themes/caseslegit/js/referrals.js
+++ wp-content/themes/caseslegit/js/referrals.js
@@ -4,20 +4,25 @@
  * and open the template in the editor.
  */
 var referralChange;
+//this first part is for Wordpress, which takes issue with dollar signs or whatever
 jQuery(function($) {
+    //meat of the script here
 referralChange = function () {
 $("input[type='checkbox']").change(function(){
     if($(this).is(":checked")){
         $(this).closest(".program-check").addClass("ref-selected"); 
-        $(this).closest(".program-check").addClass("ref-selected-box"); 
-        console.log("unrill");
+        $(this).siblings(".custom-checkbox").css('background', 'url(http://www.localhost/2015/wp-content/themes/caseslegit/images/checkon.png)');
+        console.log("four");
     }else{
         $(this).closest(".program-check").removeClass("ref-selected");
-        $(this).closest(".program-check").removeClass("ref-selected-box");
-        console.log("rollercoaster");
+        console.log("days");
+        $(this).siblings(".custom-checkbox").css('background', 'url(http://www.localhost/2015/wp-content/themes/caseslegit/images/checkoff.png)');
     }
 });
 };
+//run this script on the page load
 referralChange();
+//run it again whenever new fields appear (namely, this line is telling jQuery
+//to look for changes in the conditional fields)
 $(".caldera-forms-conditional-field").bind("DOMSubtreeModified",referralChange);
 });
